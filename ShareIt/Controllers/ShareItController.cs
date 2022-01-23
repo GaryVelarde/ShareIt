fe6d767f62b368052f77eb06f4f2f4232542045a
Gary Isaac Velarde Rios <garyl_12@hotmail.com>
2022-01-23 17:57:10 -0500

    Inicio del proyecto

---
+++ ShareIt/Controllers/ShareItController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShareIt.Controllers
+{
+    public class ShareItController : Controller
+    {
+        // GET: ShareIt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShareIt/Resources/js/Login.js
@@ -0,0 +1,26 @@
+﻿function validarLogin() {
+    var usuario = document.getElementById('ipUsuario').value;
+    var password = document.getElementById('ipPassword').value;
+    if (usuario.length == 0) {
+        $("#validacionIpEmail").addClass('alert-validate');
+    } else {
+        $("#validacionIpEmail").removeClass('alert-validate');
+    }
+
+    if (password.length == 0) {
+        $("#validacionIpPassword").addClass('alert-validate');
+    } else {
+        $("#validacionIpPassword").removeClass('alert-validate');
+    }
+    if (usuario.length > 0 && password.length > 0) {
+        alert('si');
+        return false;
+    }
+};
+function validarEmail(valor) {
+    if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)) {
+        $("#validacionIpEmail").removeClass('alert-validate');
+    } else {
+        $("#validacionIpEmail").addClass('alert-validate');
+    }
+}
