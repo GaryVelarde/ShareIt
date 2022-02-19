7dde672d9decc9ec1796e0c33459a020e930e8dc
CesJauregui <cjaureguisaavedra@gmail.com>
2022-02-19 12:04:00 -0500

    Cambios, carrousel v1

---
+++ ShareIt/Negocio/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Entidad;
+
+namespace Negocio
+{
+    internal class Login
+    {
+        internal List<ResultadoSql> n_registrarUsuario(string nombres, string apellidos, string correo, string clave, DateTime fechaNacimiento, string celular)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
